852bd249798af1271f02c7fa1b6e8c80075ccdb1
Ahmad Sayad <ahmadksayad@gmail.com>
2023-04-01 15:12:57 -0400

    initial commit

---
+++ website/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          This will be the landing page of our project.
+        </p>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
