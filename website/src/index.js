70cce819e637ed904d24f68cc3a8edb9a4dc2f42
Sam Scheck <samscheck@MacBook-Air.localdomain>
2023-04-08 22:28:33 -0400

    basic layout - sample points not showing...

--- website/src/index.js
+++ website/src/index.js
@@ -6,6 +6,10 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css"></link>
+    <link rel="stylesheet" href="node_modules/ol/ol.css"></link>
+    <script src="https://cdn.jsdelivr.net/npm/elm-pep@1.0.6/dist/elm-pep.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"></script>
     <App />
   </React.StrictMode>
 );
