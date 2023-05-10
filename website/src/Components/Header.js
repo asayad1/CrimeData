3db05a66ef82987a1cc25920efb89d6c327ddc10
Sam Scheck <samscheck@Sams-MacBook-Air-2.local>
2023-05-10 13:56:30 -0400

    added Header.* files

---
+++ website/src/Components/Header.js
@@ -0,0 +1,10 @@
+import React from "react";
+import "../CSS/Header.css"
+
+export default function Header() {
+    return (
+        <div className="header">
+            <h2><b>Team 2 - Baltimore Crime</b></h2>
+        </div>
+    )
+}
