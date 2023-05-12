dd23f79ec7e45acb05db2cad3a9a892a36d919a5
Logan Cuttler <loganc1@umbc.edu>
2023-05-12 15:30:24 -0400

    This commit adds database implementation. It connects the heatmap with the database and created a component for a graph

--- website/src/Components/App.js
+++ website/src/Components/App.js
@@ -3,13 +3,21 @@ import MapOverlay from './MapOverlay';
 import '../CSS/App.css';
 import ButtonLayout from './ButtonLayout';
 import Header from './Header';
+import Graph from './graph'
 
 export default function App() {
+	//<MapOverlay data1={points2}/>
+	//<MapOverlay data1={points2}/>
+		//<Header />
+		//<ButtonLayout />
+		//<Graph/>
 	return (
 	<div className="main">
-		<MapOverlay />
+		<MapOverlay/>
 		<Header />
+		<Graph/>
 		<ButtonLayout />
+		
 	</div>
 	);
 }
