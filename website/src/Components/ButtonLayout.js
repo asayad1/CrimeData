171b4984d915605ff0a85741b47ffc556b07f8a2
Ahmad Sayad <ahmadksayad@gmail.com>
2023-05-13 14:01:34 -0400

    Added togglable maps

--- website/src/Components/ButtonLayout.js
+++ website/src/Components/ButtonLayout.js
@@ -4,7 +4,7 @@ import "../CSS/ButtonLayout.css"
 
 export default function ButtonLayout(props) {
     return (
-		<div classNamWe='mybuttons'>
+		<div className='mybuttons'>
 			<ImgButton text="create graph" logo={require("../Images/graph.png")} func={props.buttonFuncs[0]} />
 			<ImgButton text="filter data" logo={require("../Images/filter.png")} func={props.buttonFuncs[1]}/>
 			<ImgButton text="toggle features" logo={require("../Images/pin.png")} func={props.buttonFuncs[2]}/>
