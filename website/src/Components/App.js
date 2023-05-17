2d769e0496d21ad1e0747f5cbee92fd78fac7473
Ahmad Sayad <ahmadksayad@gmail.com>
2023-05-17 15:59:58 -0400

    Added download report functionality

--- website/src/Components/App.js
+++ website/src/Components/App.js
@@ -1,24 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import MapOverlay from './MapOverlay';
 import '../CSS/App.css';
 import ButtonLayout from './ButtonLayout';
 import Header from './Header';
 import Graph from './graph'
 import Sidebar from './Sidebar';
+import axios from "axios"
+import GenPDF from './Report';
 
 export default function App() {
-/*
-		<MapOverlay/>
-		<Header />
-		<Graph/>
-		<ButtonLayout />
-*/		
 	// This holds the state between toggling points and heatmaps
 	const [isHeatmap, setIsHeatmap] = useState(false);
 	const [isOpen, setIsopen] = useState(false);
 	const [filterDatas,setFilterData] = useState('');
 	const [filterData2,setFilterData2] = useState('');
 	const [filterData3,setFilterData3] = useState('');
+	const [data, setData] = useState([]);
+
+	// Query the database 
+	axios.get('http://127.0.0.1:5000/api/data')
+		.then(response => setData(response.data))
+		.catch(error => console.error(error));
+	axios.get()
+		
 
 	//function called for moving the weapon type filters to mapOverlay
 	const handleDataChange = (newData) => {
@@ -45,14 +49,39 @@ export default function App() {
 		setIsHeatmap(!isHeatmap);
 	}
 
+	// Downloads a PDF
+	function downloadPDF() {
+		
+		// Query the database
+		let filtered_data = data.slice(0, 10000);
+		if(JSON.stringify(filterDatas) !== '[]'){
+			let i = 0
+			for(i; i < filterDatas.length; i++) {
+			  filtered_data = filtered_data.filter((item) => filterDatas.includes(item[4]))
+			}
+		  }
+		  if(JSON.stringify(filterData2) !== '[]'){
+			let i = 0
+			for(i; i < filterData2.length; i++) {
+			  filtered_data = filtered_data.filter((item) => filterData2.includes(item[3]))
+			}
+		  }
+		  if(JSON.stringify(filterData3) !== '[]'){
+			let i = 0
+			for(i; i < filterData3.length; i++) {
+			  filtered_data = filtered_data.filter((item) => filterData3.includes(item[9]))
+			}
+		  }
+
+		GenPDF(filtered_data);
+	}
 
 	return (
 		<div className="main">
 			<MapOverlay heatmap={isHeatmap} filters={filterDatas} filters2={filterData2} filters3={filterData3}/>
 			<Header />
-			<ButtonLayout buttonFuncs={[displayChart, filterData, toggleHeatmap]}/>
+			<ButtonLayout buttonFuncs={[displayChart, filterData, toggleHeatmap, downloadPDF]}/>
 			<Sidebar isOpen={isOpen} ToggleSidebar={setIsopen} onDataChanged={handleDataChange} onDataChanged2={handleDataChange2} onDataChanged3={handleDataChange3}/>
-			<Graph />
 		</div>
 	);
 }
--- website/src/Components/ButtonLayout.js
+++ website/src/Components/ButtonLayout.js
@@ -5,6 +5,7 @@ import "../CSS/ButtonLayout.css"
 export default function ButtonLayout(props) {
     return (
 		<div className='mybuttons'>
+			<ImgButton text="download report" logo={require("../Images/report.png")} func={props.buttonFuncs[3]} />
 			<ImgButton text="create graph" logo={require("../Images/graph.png")} func={props.buttonFuncs[0]} />
 			<ImgButton text="filter data" logo={require("../Images/filter.png")} func={props.buttonFuncs[1]}/>
 			<ImgButton text="toggle features" logo={require("../Images/pin.png")} func={props.buttonFuncs[2]}/>
