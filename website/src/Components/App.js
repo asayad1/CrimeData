b2fe5a99774190d4814fc49068362d4b30e9fd09
AviGlassman <avig1@umbc.edu>
2023-05-16 21:13:29 -0400

    Graphs added

--- website/src/Components/App.js
+++ website/src/Components/App.js
@@ -5,6 +5,7 @@ import ButtonLayout from './ButtonLayout';
 import Header from './Header';
 import Graph from './graph'
 import Sidebar from './Sidebar';
+import ReactModal from 'react-modal';
 
 export default function App() {
 /*
@@ -16,10 +17,24 @@ export default function App() {
 	// This holds the state between toggling points and heatmaps
 	const [isHeatmap, setIsHeatmap] = useState(false);
 	const [isOpen, setIsopen] = useState(false);
+	const [showGraph, setShowGraph] = useState(false);
+	const [isChartModalOpen, setChartModalOpen] = useState(false); // Add this line
+	const [selectedOption, setSelectedOption] = useState("");
+
+	function openChartModal() {
+		setChartModalOpen(true);
+	}
+	
+	function closeChartModal() {
+		setChartModalOpen(false);
+	}
 
 	// This is the function called when displaying charts
 	function displayChart() {
-		alert("This displays a chart")
+		//alert("This displays a chart")
+		setShowGraph(true);
+		openChartModal();
+		<Graph g={showGraph} />
 	}
 
 	// This is the function called when filtering data
@@ -31,14 +46,38 @@ export default function App() {
 		setIsHeatmap(!isHeatmap);
 	}
 
+	function handleDropdownChange(event) {
+		const selectedValue = event.target.value;
+		setSelectedOption(selectedValue);
+	}
 
 	return (
 		<div className="main">
 			<MapOverlay heatmap={isHeatmap}/>
 			<Header />
-			<ButtonLayout buttonFuncs={[displayChart, filterData, toggleHeatmap]}/>
+			<ButtonLayout buttonFuncs={[displayChart , filterData, toggleHeatmap]}/>
 			<Sidebar isOpen={isOpen} ToggleSidebar={setIsopen}/>
-			<Graph />
+			{/* <ReactModal
+				isOpen={isChartModalOpen}
+				onRequestClose={closeChartModal}
+				// Add any necessary modal styling or configuration props
+			>
+				{showGraph && <Graph showGraph={showGraph} />}
+      		</ReactModal> */}
+
+			{/* New code: ReactModal component */}
+			<ReactModal
+				isOpen={isChartModalOpen}
+				onRequestClose={closeChartModal}
+				// Add any necessary modal styling or configuration props
+			>
+				<div className="modal-content">
+				{/* New code: Dropdown */}
+				
+				{/* Updated code: Show graph inside the modal */}
+				{showGraph && <Graph />}
+				</div>
+      		</ReactModal>
 		</div>
 	);
 }
